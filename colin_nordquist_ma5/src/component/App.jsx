4de70999bc3b6be0b141655a12250aaa7c6c5605
Colin Nordquist <ckhnordquist@gmail.com>
2022-03-23 13:17:18 -0700

    finished assignment

--- colin_nordquist_ma5/src/component/App.jsx
+++ colin_nordquist_ma5/src/component/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Box from './Box'
 import Container from './Container'
 
 
@@ -7,38 +6,10 @@ function App() {
 
     return (
         <div>
-            <Container>
-                
-            </Container>
+            <Container></Container>
         </div>
     );
 }
 
 
-// function App() {
-
-
-//     const [kiwiSelected, setKiwiSelected] = useState(true);
-
-//     let isKiwiSelect = "No";
-//     if (kiwiSelected) {
-
-//         isKiwiSelect = "Yes"
-//     }
-
-//     useEffect(() => console.log("render"), [isKiwiSelect]);
-
-
-//     return (
-//         <div>
-//             <div>Is Kiwi Currently Selected?</div>
-//             {isKiwiSelect}
-//             <Box onKiwiCheck={(kiwiState) => setKiwiSelected(kiwiState)}/>
-//         </div>
-//     );
-// }
-
 export default App;
-
-
-// const kiwiFunction = (kiwiState) => setKiwiSelected(kiwiState)
--- colin_nordquist_ma5/src/component/Box.jsx
+++ colin_nordquist_ma5/src/component/Box.jsx
@@ -4,74 +4,35 @@ import './Box.css';
 
 export default function Box(props) {
 
-    const [color, setColor] = useState('white')
+    const [color, setColor] = useState('white');
 
-    
     let idName = "box";
-    if (props.position == "1") {
+    if (props.position === "1") {
         idName += '1';
-    } else if (props.position == "2") {
+    } else if (props.position === "2") {
         idName += '2';
-    } else if (props.position == "3") {
+    } else if (props.position === "3") {
         idName += "3";
-    } else if (props.position == "4") {
+    } else if (props.position === "4") {
         idName += "4";
     }
 
     let className = "box";
-    if (color === 'white') {
-        className += " " + 'white';
-    } else if (color === 'black') {
+    if (color === "white") {
+        className += " " + "white";
+    } else if (color === "black") {
         className += " " + "black";
     }
 
     function changeColor() {
-
-        if (color == 'white') {
+        if (color === 'white') {
             setColor('black');
             props.blackCount(1);
-        } else if (color == 'black') {
+        } else if (color === 'black') {
             setColor('white');
             props.blackCount(-1);
         }
     }
-    console.log(className);
 
     return (<div className={className} id={idName} onClick={() => changeColor()}></div>);
-
-
-
-    /*
-
-    const fruitList = ["kiwi", "banana", "apple", "orange"]
-
-    const [fruit, setFruit] = useState('kiwi')
-    
-    let className = "box";
-    if (fruit === 'kiwi') {
-        className += " " + 'aqua';
-    } else if (fruit === 'banana') {
-        className += " " + "yellow";
-    }
-
-    function randomFruitSelector() {
-        const randomIdx = Math.floor(Math.random() * fruitList.length);
-
-        const selectedFruit = fruitList[randomIdx];
-
-        setFruit(selectedFruit);
-
-        // (kiwiState) => setKiwiSelected(kiwiState)
-        if (selectedFruit == 'kiwi') {
-            props.onKiwiCheck(true);
-        } else {
-            props.onKiwiCheck(false);
-        }
-    }
-
-    return (<div className={className}>
-        {fruit}
-        <button onClick={() => randomFruitSelector()}>Random fruit selector</button>
-    </div>);
-    */
 }
